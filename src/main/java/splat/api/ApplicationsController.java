290c195755ac0ead9ca6efd5e5b8e7983f3da5c5
Robin Tegg <robin@tegg.me.uk>
2018-11-26 20:49:41 +0000

    handle same named applications

--- src/main/java/splat/api/ApplicationsController.java
+++ src/main/java/splat/api/ApplicationsController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -46,6 +47,7 @@ public class ApplicationsController {
 	}
 
 	@ExceptionHandler(Exception.class)
+	@ResponseStatus
 	@ResponseBody
 	public ApplicationUploadCreatedResource handleException(Exception e) {
 		return new ApplicationUploadCreatedResource("You failed to create an application " + e.getMessage() + "!");
